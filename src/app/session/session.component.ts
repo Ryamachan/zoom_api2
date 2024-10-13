14feb5684df0954a922b61f11d33a8126ba5a0ba
Ryamachan <50526564+Ryamachan@users.noreply.github.com>
2024-10-13 20:29:08 +0900

    mdofigy

--- src/app/session/session.component.ts
+++ src/app/session/session.component.ts
@@ -23,7 +23,7 @@ export class SessionComponent implements OnInit {
       this.userName = params['config'].userName;
       this.sessionName = params['config'].sessionName; // ここに実際のミーティングIDを設定
       this.password = params['config'].password;
-      console.log(JSON.stringify(params['config']));
+      console.table(params['config']);
     });
 
     this.startVideoSession();
