acfd9d1d77d49adfb6f44efd51bc87bceef312c5
Ryamachan <50526564+Ryamachan@users.noreply.github.com>
2024-11-04 02:15:49 +0900

    new way

--- src/app/join/join.component.ts
+++ src/app/join/join.component.ts
@@ -29,14 +29,6 @@ export class JoinComponent {
     this.checkScreenSharingSupport();
   }
 
-  // スクリーンシェアサポート確認
-  checkScreenSharingSupport() {
-    if ('getDisplayMedia' in navigator.mediaDevices) {
-      console.log('Screen sharing is supported in this browser.');
-    } else {
-      console.error('Screen sharing is not supported in this browser.');
-    }
-  }
 
   getCookie(name: string): string | null {
     const matches = document.cookie.match(new RegExp(
@@ -51,52 +43,7 @@ export class JoinComponent {
 
 
   joinSession() {
-    this.sessionContainer = this.document.getElementById('sessionContainer');
-    this.inSession = true;
-
-    // CSRFトークンを取得してからAPIリクエストを送る
-    this.getCsrfToken().subscribe({
-      next: () => {
-        const xsrfToken = this.getCookie('XSRF-TOKEN');  // XSRF-TOKENをクッキーから取得
-
-        if (xsrfToken) {
-          // CSRFトークン取得後に署名を取得
-          this.httpClient.post(this.authEndpoint, {
-            role: this.role,
-          }, {
-            withCredentials: true,
-            headers: {
-              'Content-Type': 'application/json',
-              'X-XSRF-TOKEN': xsrfToken  // ヘッダーにXSRF-TOKENを設定
-            }
-          }).subscribe({
-            next: (data: any) => {
-              if (data.signature) {
-                console.log(data.signature);
-                this.config.videoSDKJWT = data.signature;
-                this.config.sessionName = data.sessionName;
-                this.config.userName = data.userName;
-                this.router.navigate(['/session', {
-                  jwt: this.config.videoSDKJWT,
-                  name: this.config.userName,
-                  sessionName: this.config.sessionName
-                }]);
-
-              } else {
-                console.error('Invalid response', data);
-              }
-            },
-            error: (error: any) => {
-              console.error('Error fetching JWT', error);
-            }
-          });
-        } else {
-          console.error('CSRF token not found in cookies');
-        }
-      },
-      error: (error) => {
-        console.error('Error fetching CSRF token', error);
-      }
+      this.router.navigate(['/session']);
     });
   }
 
