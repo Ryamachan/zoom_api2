aa5051b81dd797e840b152909b1ad0a134de50b6
Ryamachan <50526564+Ryamachan@users.noreply.github.com>
2024-11-19 08:21:46 +0900

    mdofiyg

--- src/app/session/session.component.ts
+++ src/app/session/session.component.ts
@@ -70,6 +70,7 @@ export class SessionComponent implements OnInit {
 
     const csrfToken = this.videoService.getCookie('XSRF-TOKEN') || ''; // nullの場合は空文字を代入
 
+    console.log(formData);
     this.httpClient.post('https://d39pgh50coc0c9.cloudfront.net/api/process-video', formData, {
       headers: { 'X-XSRF-TOKEN': csrfToken },
       withCredentials: true
